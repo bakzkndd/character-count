97f3fbb7f57afe247b10f571c0039c5ae32ad6d5
bakzkndd <50497725+bakzkndd@users.noreply.github.com>
2021-06-11 18:32:51 +0200

    revert useless changes

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const { inject, uninject } = require("powercord/injector");
 module.exports = class CharacterCount extends Plugin {
   async startPlugin() {
 
-    this.loadStylesheet('./style.css')
-
-    const ChannelTextAreaContainer = await getModule(m => m.default && m.default.displayName === 'SlateCharacterCount');
+    const ChannelTextAreaContainer = await getModule(m => m.default && m.default.displayName === 'SlateCharacterCount', false);
     
-    inject('CharacterCount', getModule(m => m?.default?.displayName === 'SlateCharacterCount'), 'default', (args, res) => {
+    inject('CharacterCount', ChannelTextAreaContainer, 'default', (args, res) => {
 
-      return <div className= {`${getModule('characterCount', 'upsell').characterCount} characterCount`}>{`Characters Typed: ${args[0].currentLength}`}</div>
-      
+      return React.createElement(
+        'div', 
+        {className: "CharacterCount", id: "CharacterCount"},
+        `Characters Typed: ${args[0].currentLength}`
+      )
 
     });
 
